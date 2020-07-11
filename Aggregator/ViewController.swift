bd90c4e35d9f6d470d1173bf1c7fdcd40d08e2af
Влад Яцив <mtb.vl.98@gmail.com>
2020-07-11 16:55:32 +0300

    Initial Commit

---
+++ Aggregator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aggregator
+//
+//  Created by Владислав on 11.07.2020.
+//  Copyright © 2020 Yatts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
