f92d8b6daf8b6abbc3cdbcd15b52599ddc581bcc
Влад Яцив <mtb.vl.98@gmail.com>
2020-07-21 19:42:21 +0300

    ver 0.01

--- Aggregator/ViewController.swift
+++ Aggregator/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextViewDelegate {
+    
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var dataEmailPlaceholder: UITextField!
+    @IBOutlet weak var dataPasswodPlaceholder: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        loginButton.layer.cornerRadius = 7 // Радиус кнопки
+        // Vlad: dataEmailPlaceholder
+        let dataEmailPlace = UITextView.self
+        dataEmailPlaceholder.delegate = self as? UITextFieldDelegate
+        func dataEmailPlaceholderWasBeginEditing(_ textView: UITextView) {
+            if textView.text == "Почта" {
+                textView.text = ""
+                textView.returnKeyType = .done
+                textView.textColor = UIColor.black
+            }
+        }
+        func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacmentText text: String) -> Bool {
+            if text == "\n" {
+                textView.resignFirstResponder()
+            }
+            return true
+        }
+        func textViewDidEndEditing(_ textView: UITextView) {
+            textView.text = "Почта"
+            dataEmailPlaceholder.textColor = UIColor.lightGray
+        }
+        // Vlad: dataPasswordPlaceholder
+        let dataPasswordPlaceholder = UITextView.self
     }
-
-
 }
+
+
 
